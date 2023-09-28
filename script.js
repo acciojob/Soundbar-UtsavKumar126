9fbb8fed0e5c4ce1702e0f1c7b929dc8ac085f48
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-28 20:53:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let buttons=document.getElementsByClassName("btn");
+let sound='';
+for(let i=0;i<buttons.length;i++){
+	buttons[i].addEventListener("click",(event)=>{
+		let text=event.target.textContent;
+		sound=new Audio(`./sound/${text}`);
+		sound.play();
+	})
+}
+let stop=document.getElementsByClassName("stop")[0];
+stop.addEventListener("click",stopmusic);
+function stopmusic() {
+	sound.pause();
+}
